3bac11c64c65212142c2e95a1cdccefb7c8b6304
papertiger <wanghz518@gmail.com>
2024-02-29 14:20:27 +0800

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,38 @@
+import yaml
+
+# 要过滤的字符串列表
+filter_strings = ["hysteria2", "hysteria", "trojan"]
+
+# 过滤含有指定字符串的条目
+def should_filter(entry):
+    return any(filter_string in str(entry) for filter_string  in filter_strings)
+
+
+def filter_yaml_file(input_file):
+    with open(filename, 'r') as file:
+        content = file.read()
+    data = content.replace("::", ":")
+
+    # 使用列表推导式过滤出满足条件的项目
+    filtered_data = [item for item in data['proxies'] if item.get('type') in filter_strings ]
+    vmess = [item for item in data['proxies'] if item.get('type') == 'vmess']
+
+    # 打印过滤后的结果
+    print(len(filtered_data), " 个代理")
+    print(len(vmess), " 个vmess")
+
+    # 将结果写回YAML文件
+    output_file = 'htonly.yml'
+    output_vmess = 'vmess.yml'
+    with open(output_file, 'w') as file:
+        # 写入 "proxies:" 头部
+        file.write("proxies:\n")
+        yaml.dump(filtered_data, file)
+    with open(output_vmess, 'w') as file:
+        # 写入 "proxies:" 头部
+        file.write("proxies:\n")
+        yaml.dump(vmess, file)
+        
+# 示例用法
+input_file = 'clash.yaml'
+filter_yaml_file(input_file)
