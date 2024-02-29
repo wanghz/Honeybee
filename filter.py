1214027ee70c1072eed5dfda8a53b1b98dabcb59
papertiger <wanghz518@gmail.com>
2024-02-29 14:47:09 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,38 +1,57 @@
 import yaml
+import urllib.request
 
-# 要过滤的字符串列表
-filter_strings = ["hysteria2", "hysteria", "trojan"]
+def gathering_clash():
+    url = "https://api.v1.mk/sub?target=clash&url=https%3A%2F%2Fpp.dcd.one%2Fclash%2Fproxies%3Fspeed%3D10%7Chttps%3A%2F%2Frvorch.treze.cc%2Fclash%2Fproxies%3Fspeed%3D10%7Chttps%3A%2F%2Fkiwi2.cgweb.top%2Fclash%2Fproxies%3Fspeed%3D10"    
+    hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
+        
+    req = urllib.request.Request(url, headers=hdr)
+    response = urllib.request.urlopen(req)
+    content = response.read().decode('utf-8')
+
+    #源数据中有非法字符，修改一下
+    data = content.replace("::", ":")
+
+    return data
+
 
 # 过滤含有指定字符串的条目
 def should_filter(entry):
     return any(filter_string in str(entry) for filter_string  in filter_strings)
 
 
-def filter_yaml_file(input_file):
-    with open(input_file, 'r', encoding='utf-8') as file:
-        content = file.read()
-    data = content.replace("::", ":")
+def filter_yaml_file():
+    data = gathering_clash()
+
+    # 要过滤的字符串列表
+    filter_strings = ["hysteria2", "hysteria", "trojan"]
 
     # 使用列表推导式过滤出满足条件的项目
-    filtered_data = [item for item in data['proxies'] if item.get('type') == "hysteria2" or item.get('type') == "hysteria" or item.get('type') == "trojan"]
+    #filtered_data = [item for item in data['proxies'] if item.get('type') == "hysteria2" or item.get('type') == "hysteria" or item.get('type') == "trojan"]
+    filtered_data = [item for item in data['proxies'] if item.get('type') in filter_strings]
     vmess = [item for item in data['proxies'] if item.get('type') == 'vmess']
 
     # 打印过滤后的结果
     print(len(filtered_data), " 个代理")
     print(len(vmess), " 个vmess")
 
-    # 将结果写回YAML文件
-    output_file = 'htonly.yml'
-    output_vmess = 'vmess.yml'
-    with open(output_file, 'w', encoding='utf-8') as file:
-        # 写入 "proxies:" 头部
-        file.write("proxies:\n")
-        yaml.dump(filtered_data, file)
-    with open(output_vmess, 'w', encoding='utf-8') as file:
-        # 写入 "proxies:" 头部
-        file.write("proxies:\n")
-        yaml.dump(vmess, file)
-        
-# 示例用法
+    return  filtered_data, vmess
+
+
+# Main
 input_file = 'clash.yaml'
-filter_yaml_file(input_file)
+
+htonly, vmess = filter_yaml_file()
+
+# 将结果写回YAML文件
+output_file = 'htonly.yml'
+output_vmess = 'vmess.yml'
+with open(output_file, 'w', encoding='utf-8') as file:
+    # 写入 "proxies:" 头部
+    file.write("proxies:\n")
+    yaml.dump(htonly, file)
+with open(output_vmess, 'w', encoding='utf-8') as file:
+    # 写入 "proxies:" 头部
+    file.write("proxies:\n")
+    yaml.dump(vmess, file)
+    
