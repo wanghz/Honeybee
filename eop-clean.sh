25f47019a05dbdd87e0eac8d75eebe2c70750ec8
papertiger <wanghz518@gmail.com>
2024-04-16 08:28:30 +0800

    Update eop-clean.sh

--- eop-clean.sh
+++ eop-clean.sh
@@ -17,6 +17,7 @@ urls=(
   "https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Config%20list10.txt"
 )
 
+cd ./sub
 # Function to download and filter text files
 download_and_filter() {
     url="$1"
@@ -34,12 +35,13 @@ for url in "${urls[@]}"; do
     download_and_filter "$url"
 done
 
+cd ..
 # download some others
-curl -s "https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/singbox/mix.json" -o "mix.json"
-curl -s "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/singbox/sfasfi/mixLite.json" -o "mixlite.json"
-curl -s "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/clash-meta/actives_under_1000ms.yaml" -o "under1k.yml"
-curl -s "https://raw.githubusercontent.com/lagzian/SS-Collector/main/vmess.txt" -o "vmess.txt"
-curl -s "https://raw.githubusercontent.com/lagzian/SS-Collector/main/reality.txt" -o "reality.txt"
+curl -s "https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/singbox/mix.json" -o "./sub/mix.json"
+curl -s "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/singbox/sfasfi/mixLite.json" -o "./sub/mixlite.json"
+curl -s "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/clash-meta/actives_under_1000ms.yaml" -o "./sub/under1k.yml"
+curl -s "https://raw.githubusercontent.com/lagzian/SS-Collector/main/vmess.txt" -o "./sub/vmess.txt"
+curl -s "https://raw.githubusercontent.com/lagzian/SS-Collector/main/reality.txt" -o "./sub/reality.txt"
 
 # do some edit
 python editjson.py  mix.json
