64f2174fd10632c172457a74c369473d52ef3d87
papertiger <wanghz518@gmail.com>
2024-05-23 11:28:59 +0800

    Update sbfromtxt.sh

--- sing-box-sub/sbfromtxt.sh
+++ sing-box-sub/sbfromtxt.sh
@@ -16,14 +16,13 @@ files=(
 counter=1
 for file in "${files[@]}"; do
     # 在这里添加您的提取逻辑
-    localfile="file:///../sub/$file"
+    localfile="https://raw.githubusercontent.com/wanghz/Honeybee/main/sub/$file"
     echo "Extracting from local file: $localfile"
     filename="./f${counter}.json"
     echo "Saving to file: $filename"
     jq --arg localfile "$localfile" --arg filename "$filename" '.subscribes[0].url = $localfile | .save_config_path = $filename' provx.json > tmpfile && mv tmpfile provx.json
     python ./newmain.py -c provx.json
     ((counter++))
-    
 done
 
 
