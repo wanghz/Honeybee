06efb560ecf03ca7d11b7b07095fbc9c6370bfdf
papertiger <wanghz518@gmail.com>
2024-05-23 10:30:41 +0800

    Create sbfromtxt.sh

---
+++ sing-box-sub/sbfromtxt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+files=(
+        "Config%20list1.txt"
+        "Config%20list10.txt"
+        "Config%20list2.txt"
+        "Config%20list3.txt"
+        "Config%20list4.txt"
+        "Config%20list5.txt"
+        "Config%20list6.txt"
+        "Config%20list7.txt"
+        "Config%20list8.txt"
+        "Config%20list9.txt"
+)
+
+counter=1
+for file in "${files[@]}"; do
+    echo "Extracting from URL: $url"
+    # 在这里添加您的提取逻辑
+    localfile = "file:///home/runner/work/Honeybee/Honeybee/sub/$file"
+    filename="./f${counter}.json"
+    echo "Saving to file: $filename"
+    jq --arg url "$url" --arg filename "$filename" '.subscribes[0].url = $localfile | .save_config_path = $filename' provx.json > tmpfile && mv tmpfile provx.json
+    python ./newmain.py -c provx.json
+    ((counter++))
+done
+
+
+
