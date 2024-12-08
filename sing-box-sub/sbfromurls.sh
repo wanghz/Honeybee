3fdaa8fa6f2cbbcb2e732a6cc46dd34966f34e72
papertiger <wanghz518@gmail.com>
2024-12-08 09:16:45 +0800

    Update sbfromurls.sh

--- sing-box-sub/sbfromurls.sh
+++ sing-box-sub/sbfromurls.sh
@@ -3,10 +3,12 @@
 # 定义两个文件的 URL
 snakem982url1="https://raw.githubusercontent.com/snakem982/proxypool/refs/heads/main/source/nodelist.txt"
 snakem982url2="https://raw.githubusercontent.com/snakem982/proxypool/refs/heads/main/source/proxies.txt"
+local="https://raw.githubusercontent.com/wanghz/Honeybee/refs/heads/main/sing-box-sub/sub_urls.txt"
 
 # 下载文件
 curl -o snakem982url1.txt "$snakem982url1"
 curl -o snakem982url2.txt "$snakem982url2"
+curl -o localurl.txt "$local"
 
 # 初始化数组
 urls=()
@@ -14,7 +16,7 @@ urls=()
 # 将文件内容合并到数组中
 while IFS= read -r line; do
     urls+=("$line")
-done < <(cat file1.txt file2.txt)
+done < <(cat snakem982url1.txt snakem982url2.txt localurl.txt)
 
 counter=1
 for url in "${urls[@]}"; do
