cca9c10c0a7990914be98b0ce6489eed46138474
papertiger <wanghz518@gmail.com>
2024-12-12 10:11:25 +0800

    Update sbfromurls.sh

--- sing-box-sub/sbfromurls.sh
+++ sing-box-sub/sbfromurls.sh
@@ -9,18 +9,27 @@ local="https://raw.githubusercontent.com/wanghz/Honeybee/refs/heads/main/sing-bo
 curl -o snakem982url1.txt "$snakem982url1"
 curl -o snakem982url2.txt "$snakem982url2"
 curl -o localurl.txt "$local"
+# 添加一些
 echo "https://raw.gitmirror.com/Memory2314/VMesslinks/main/links/vmess" >> localurl.txt
 echo "https://raw.githubusercontent.com/a2470982985/getNode/main/clash.yaml" >> localurl.txt
 echo "https://raw.githubusercontent.com/Huibq/TrojanLinks/master/links/vmess" >> localurl.txt
 echo "https://raw.githubusercontent.com/Huibq/TrojanLinks/master/links/trojan" >> localurl.txt
 
+# 删除一些找不到的或没内容的
+sed /i 's|https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/base64/mix|/d' nodelist.txt
+sed /i 's|https://raw.githubusercontent.com/Vauth/node/main/Master|/d' nodelist.txt
+sed /i 's|http://174.137.58.32:12580/clash/proxies|/d' proxies.txt
+sed /i 's|http://104.168.244.47:12580/clash/proxies|/d' proxies.txt
+sed /i 's|http://beetle.lander.work/clash/proxies|/d' proxies.txt
+sed /i 's|https://proxy.fldhhhhhh.top/clash/proxies|/d' proxies.txt
+sed /i 's|https://raw.gitmirror.com/Memory2314/VMesslinks/main/links/vmess|/d' localurl.txt
+
 # 初始化数组
 urls=()
-
 # 将文件内容合并到数组中
 while IFS= read -r line; do
     urls+=("$line")
-done < <(cat snakem982url1.txt snakem982url2.txt localurl.txt)
+done < <(awk '1; ENDFILE {print ""}' snakem982url1.txt snakem982url2.txt localurl.txt)
 
 counter=1
 for url in "${urls[@]}"; do
