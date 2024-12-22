652d4d8446dcd98bcc4cb14ca8f7af68ee7fc91d
papertiger <wanghz518@gmail.com>
2024-12-22 13:23:44 +0800

    Update sbfromtxt.sh

--- sing-box-sub/sbfromtxt.sh
+++ sing-box-sub/sbfromtxt.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
 files=(
-        "split_aa"
-        "split_ab"
-        "split_ac"
-        "split_ad"
-        "split_ae"
-        "split_af"
-        "split_ag"
-        "split_ah"
-        "split_ai"
-        "split_aj"
         "split_act"
         "split_1k"
         "htonly.yml"
 )
 
 # 使用 find 匹配以 "split" 开头的文件
-files_split=$(find . -maxdepth 1 -type f -name "split*" | sed 's|^\./||')
+files_split=$(find /home/runner/work/Honeybee/Honeybee/sub -maxdepth 1 -type f -name "split*" | sed 's|^\./||')
 all_files="$files_split $files"
 
 counter=1
-for file in "${files[@]}"; do
+for file in "${all_files[@]}"; do
     # 在这里添加您的提取逻辑
     localfile="https://raw.githubusercontent.com/wanghz/Honeybee/main/sub/$file"
     echo "Extracting from local file: $localfile"
