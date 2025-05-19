39215fd1b4313cad72906d86f97268b352bea393
papertiger <wanghz518@gmail.com>
2025-05-19 14:47:21 +0800

    Update eop-clean.sh

--- eop-clean.sh
+++ eop-clean.sh
@@ -76,6 +76,18 @@ cd ..
 curl -s "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/refs/heads/master/collected-proxies/row-url/actives.txt" -o "./sub/split_act"
 curl -s "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/clash-meta/actives_under_1000ms.yaml" -o "./sub/split_1k"
 
+wget https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip
+unzip pup_v0.4.0_linux_amd64.zip
+sudo mv pup /usr/local/bin/
+
+URL="https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+# 使用 curl 获取网页内容，并用 pup 提取指定元素
+curl -s "$URL" | pup '#wiki-body > div > div:nth-child(22) > pre text{}' >>split_act
+curl -s "$URL" | pup '#wiki-body > div > div:nth-child(27) > pre text{}' >>split_act
+
+URL="https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+curl -s "$URL" | pup '#wiki-body > div > div:nth-child(25) > pre text{}' >>split_act
+
 # do some editing
 cd ./sub
 cp ../editjson.py .
