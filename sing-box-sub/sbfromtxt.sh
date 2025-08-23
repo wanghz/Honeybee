aa3c87da6400d3ebc53eebcb76c942a0862c7f85
papertiger <wanghz518@gmail.com>
2025-08-23 16:54:12 +0800

    Update sbfromtxt.sh

--- sing-box-sub/sbfromtxt.sh
+++ sing-box-sub/sbfromtxt.sh
@@ -23,7 +23,6 @@ done
 
 links=(
         "https://raw.githubusercontent.com/Alvin9999/pac2/master/hysteria2/config.json"
-        "https://chromego-sub.netlify.app/sub/base64.txt"
         "https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list.txt" 
         "https://raw.githubusercontent.com/PuddinCat/BestClash/refs/heads/main/proxies.yaml" 
         "https://raw.githubusercontent.com/Barabama/FreeNodes/refs/heads/main/nodes/clashmeta.txt"
