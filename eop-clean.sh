ec9ef062686ad8abc2cf696f71ec5c796f2fc36e
papertiger <wanghz518@gmail.com>
2025-08-26 15:23:17 +0800

    Update eop-clean.sh

--- eop-clean.sh
+++ eop-clean.sh
@@ -7,10 +7,10 @@ currentyears=$(date +%Y)
 urls=(
   #"https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/vless.txt"
   #"https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/vmess.txt"
-  "https://raw.githubusercontent.com/Misaka-blog/chromego_merge/refs/heads/main/sub/merged_proxies_new.yaml"
-  "https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/trojan.txt"
-  "https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/vless.txt"
-  "https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/vmess.txt"
+  #"https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/trojan.txt"
+  #"https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/vless.txt"
+  #"https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/Splitted-By-Protocol/vmess.txt"
+  "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/refs/heads/master/collected-proxies/clash-meta/all.yaml"
   "https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/ss.txt"
   "https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vless.txt"
   "https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vmess.txt"
@@ -78,50 +78,20 @@ for url in "${urls[@]}"; do
 done
 
 cd ..
-# download some others
-curl -s "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/refs/heads/master/collected-proxies/row-url/actives.txt" -o "./sub/split_act"
-curl -s "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/clash-meta/actives_under_1000ms.yaml" -o "./sub/split_1k"
 
-wget https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip
-unzip pup_v0.4.0_linux_amd64.zip
-sudo mv pup /usr/local/bin/
+#wget https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip
+#unzip pup_v0.4.0_linux_amd64.zip
+#sudo mv pup /usr/local/bin/
 
-URL="https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-# 使用 curl 获取网页内容，并用 pup 提取指定元素
-CONTENT=$(curl -s "$URL" | pup '#wiki-body > div > div:nth-child(22) > pre text{}')
-if [ -n "$CONTENT" ]; then
-  echo "$CONTENT" 
-  echo "$CONTENT" >> ./sub/split_act
-else
-  echo "No content extracted" >&2
-fi
-
-CONTENT=$(curl -s "$URL" | pup '#wiki-body > div > div:nth-child(27) > pre text{}')
-if [ -n "$CONTENT" ]; then
-  echo "$CONTENT"  | tr -d '\n'
-  echo "$CONTENT" >> ./sub/split_act
-else
-  echo "No content extracted" >&2
-fi
-
-URL="https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-CONTENT=$(curl -s "$URL" | pup '#wiki-body > div > div:nth-child(25) > pre text{}')
-if [ -n "$CONTENT" ]; then
-  echo "$CONTENT" 
-  echo "$CONTENT" >> ./sub/split_act
-else
-  echo "No content extracted" >&2
-fi
-rm -f pup_*.zip.*
-rm -f split_alv
+#rm -f pup_*.zip.*
+#rm -f split_alv
 # do some editing
-cd ./sub
-cp ../editjson.py .
+#cd ./sub
+#cp ../editjson.py .
 
 # cleaning
 # 定义要检查和删除的文件列表
 #rm -rf ./Config%20list*.txt
 
-cd ..
 # end
 echo bye~
